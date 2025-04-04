2f2de1c34b0bfe83ed9c655e90ffe122951de8ff
brandonmdiaz <brandonmanueldiazflores@gmail.com>
2025-04-04 09:37:52 -0600

    test file input

--- scripts/transform.ts
+++ scripts/transform.ts
@@ -2,8 +2,11 @@ const fs = require("fs");
 const cheerio = require("cheerio");
 
 const outputFile = "./extractedData.json";
+const inputFile = "/consent/test.html";
 
 const json = { "hello": "world" }
+const test = fs.readFileSync(inputFile, 'utf-8');
+console.log(test);
 
 fs.writeFileSync(outputFile, JSON.stringify({ json }, null, 2));
 
